fcd324522cff4debcc939f28b5d1d2d1f0732e4c
19Lida <lida19melnyk@gmail.com>
2022-11-20 22:08:16 +0200

    додає таск 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+// генерування випадкового кольору
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+
+//  додаємо слухачів подій
+refs.btnStart.addEventListener('click', onClickShowColor);
+refs.btnStop.addEventListener('click', onClickStopColor);
+
+// функція показу кольору
+function onClickShowColor() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+  timerId = setInterval(() => {
+    refs.body.style.background = getRandomHexColor();
+  }, 1000);
+}
+// функція Стоп кольору
+function onClickStopColor() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+  clearInterval(timerId);
+}
