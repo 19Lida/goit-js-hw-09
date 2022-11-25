27b58dcfe7cd48ef4d2df9db40f14372f8a0fa73
19Lida <lida19melnyk@gmail.com>
2022-11-25 22:20:42 +0200

    редагує таск 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,50 +3,39 @@ import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+
 const refs = {
-  btnStart: document.querySelector('button[data-start]'),
+  dateForm: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('[data-start]'),
+  timer: document.querySelector('.timer'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 };
-
-class Timer {
-  constructor({ onTick }) {
-    this.intervalId = null;
-    this.isActive = false;
-    this.onTick = onTick;
-    this.init();
-  }
-  start() {
-    if (this.isActive) {
-      return;
-    }
-    init() {
-      const time = convertMs(0);
-      this.onTick(time);
-    }
-
-    const startTime = Date.now();
-    this.isActive = true;
-
-    this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = startTime - currentTime;
-      const time = this.convertMs(deltaTime);
-      // console.log(time);
-      this.onTick(time);
-    }, 1000);
-  }
-  stop() {
-    clearInterval(this.intervalId);
-    this.isActive = false;
-  }
-
-}
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
+let timerId = null;
+// дата, вибрана користувачем
+let userDate = null;
+// об"єкт опшинів(все,що міститься у змінній options),які ми передаємо до екземпляра flatpickr
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  //вбудований метод бібліотеки,який буде викликатися при закритті календаря
+  onClose(selectedDates) {
+    selectedDates[0] <= options.defaultDate
+      ? (Notiflix.Notify.failure('Please choose a date in the future'),
+        // у випадку невдачі робить кнопку неактивною
+        (refs.btnStart.disabled = true))
+      : (Notiflix.Notify.success('The countdown has started'),
+        // у випадку успіху робить кнопку активною
+        (refs.btnStart.disabled = false));
+    userDate = selectedDates[0];
+  },
+};
+// функція,яка переводить у тижні,дні,години,секунди...готова з умови ДЗ
+const convertMs = ms => {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
@@ -62,49 +51,31 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+};
+// повертає різницю між обраною в календарі і поточною датою
+const getDifferenceDate = () => userDate - new Date();
+//додає нуль до одинарних чисел
+const addLeadingZero = value => {
+  return String(value).padStart(2, '0');
+};
+// записує різницю дат в HTML
+const setTimeToHTML = () => {
+  const { days, hours, minutes, seconds } = convertMs(getDifferenceDate());
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+};
+// на кнопку старт вішаємо слухача подій,який слухає клік і викликає хендлер функцію,яка перевіряє,чи є різниця між поточною датою і обраною.Якщо немає,то видаляє сетінтервал,а якщо різниця є,то викликає 1 за сек.функцію,яка оновлює дані в HTML
+refs.btnStart.addEventListener('click', onStartTimer);
+refs.btnStart.disabled = true;
+flatpickr(refs.dateForm, options);
+function onStartTimer(evt) {
+  timerId = setInterval(() => {
+    if (getDifferenceDate() <= 0) {
+      clearInterval(timerId);
+      return;
+    }
+    setTimeToHTML();
+  }, 1000);
 }
-
-addLeaingZero(value) {
-  return String(value).padStart(2, 0)
-}
-const timer = new Timer({
-  onTick: updateClockFase,
-});
-
-
-refs.btnStart.addEventListener('click', onBtnClickStart);
-
-function updateClockFase({ days, hours, minutes, seconds }) {
-  refs.clockface.textContent = `${days}: ${hours}: ${minutes}: ${seconds}`;
-}
-function onBtnClickStart() {
-  timer.start();
-}
-// const timer = {
-//   intervalId: null,
-//   isActive: false,
-
- 
-// start() {
-//   if (this.isActive) {
-//     return;
-//   }
-
-//     const startTime = Date.now();
-//     this.isActive = true;
-
-//     this.intervalId = setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = startTime - currentTime;
-//       const time = this.convertMs(deltaTime);
-//       // console.log(time);
-//       this.onTick(time);
-//     }, 1000);
-//   }
-//   stop() {
-//     clearInterval(this.intervalId);
-//     this.isActive = false;
-//   }
-
-// const timer = new Timer();
-
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+const refs = {
+  formEl: document.querySelector('form'),
+  delayEl: document.querySelector('input[name="delay"]'),
+  stepEl: document.querySelector('input[name="stap"]'),
+  amountEl: document.querySelector('input[name="amount"]'),
+};
+refs.formEl.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let { delay, stap, amount } = onGettingData();
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    // delay = +step;
   }
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+function onGettingData() {
+  return {
+    delay: Number(refs.delayEl.value),
+    step: Number(refs.stepEl),
+    amount: Number(refs.amountEl.value),
+  };
 }
